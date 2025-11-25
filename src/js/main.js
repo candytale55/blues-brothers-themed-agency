7da9e478dc165fbdd3332b1c170fc9ed25ed36ff
CandyTale55 <candytale55@gmail.com>
2025-11-26 00:22:19 +0100

    feat(navigatio): implement mobile navigation toggle functionality from open to close. - burger menu to open is still pending.

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,12 @@ const copyYear = document.querySelector(".copy-year");
 copyYear.textContent = new Date().getFullYear();
 // TODO: Make this a function? 
   
+
+// ========== TOGGLE NAVIGATION ==========
+
+const closeNavBtn = document.querySelector(".close-nav-btn");
+const headerNav = document.querySelector(".header-nav");
+
+closeNavBtn.addEventListener("click", () => {
+    headerNav.classList.remove('navigation-open'); 
+});
